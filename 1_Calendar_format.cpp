60fe0fd591d1f1c60ab77d8f87c8ed8e04f86987
siberus <mr.siberus@yandex.ru>
2023-05-21 19:51:01 +0700

    First commit

---
+++ 1_Calendar_format.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+
+int main() 
+{
+    int nDays;
+    string weekday;
+    cin >> nDays >> weekday;
+    array<string, 7> weekdays{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+    
+
+    return 0;
+}
