e64b24ba783ae66184e788d3c4f32b3cb9eafbb2
siberus <mr.siberus@yandex.ru>
2023-05-21 20:39:07 +0700

    Final commit

--- 1_Calendar_format.cpp
+++ 1_Calendar_format.cpp
@@ -1,16 +1,45 @@
 #include <iostream>
 #include <array>
+#include <iomanip>
 
 using namespace std;
+array<string, 7> g_weekdays{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
 
+int emptyDays(string p_weekday)
+{
+    for (int indexWeekday = 0; indexWeekday < 7; indexWeekday++)
+    {
+       if (g_weekdays[indexWeekday] == p_weekday)
+       {
+            for (int numberEmptyDays = 0; numberEmptyDays < indexWeekday; numberEmptyDays++)
+            {
+                cout << ".. ";
+            }
+            return indexWeekday;
+       }
+    }
+    return 0;
+}
+
+void outputDate(int p_startIndexDay, int p_nDays)
+{
+    for (int dayNumber = 0; dayNumber < p_nDays; dayNumber++)
+    {
+        cout << setfill('.') << setw(2) << dayNumber+1 << " ";
+        if (!((dayNumber+1+p_startIndexDay)%7)) 
+            cout << endl;
+    }
+    return;
+}
 
 int main() 
 {
     int nDays;
     string weekday;
     cin >> nDays >> weekday;
-    array<string, 7> weekdays{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
-    
+    int startIndexDay;
+    startIndexDay = emptyDays(weekday);
+    outputDate(startIndexDay, nDays); 
 
     return 0;
 }
